3afd4efedde55f5eb5de826cd9f96bca7165ff01
C1325543 Matheus Romano Braga <romanobraga.m@gmail.com>
2022-06-17 19:03:29 -0300

    feat: other modules

--- js/professores.js
+++ js/professores.js
@@ -44,6 +44,42 @@ $(document).ready(function () {
     });
   });
 
+  $("#presencaTable").bootstrapTable({
+    data: jsonPresenca
+  });
+
+  $("#presencaInput").on("keyup", function () {
+    var presenca = $(this).val().toLowerCase();
+    $("#presencaTable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(presenca) > -1);
+    });
+  });
+
+  $("#notasTable").bootstrapTable({
+    data: jsonNotas
+  });
+
+  $("#relatorioTable").bootstrapTable({
+    data: jsonRelatorios
+  });
+
+  $("#relatorioInput").on("keyup", function () {
+    var relatorio = $(this).val().toLowerCase();
+    $("#relatorioTable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(relatorio) > -1);
+    });
+  });
+
+  $("#caTable").bootstrapTable({
+    data: jsonCtrlAtividades
+  });
+
+  $("#caInput").on("keyup", function () {
+    var relatorio = $(this).val().toLowerCase();
+    $("#caTable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(relatorio) > -1);
+    });
+  });
 
   
 });
@@ -218,5 +254,122 @@ var jsonMatricula =
     "material": "inserir material",
     "docente": "Prof. Marcos",
     "data": "01/01/2019",
+  },
+];
+
+var jsonPresenca =
+[
+  {
+    "turma": "23A",
+    "n_matricula": "1",
+    "nome": "João",
+    "disciplina": "Matemática",
+    "confirmacao_presenca": "Sim ou nao",
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+  {
+    "turma": "23B",
+    "n_matricula": "2",
+    "nome": "Maria",
+    "disciplina": "Português",
+    "confirmacao_presenca": "Sim ou nao",
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+  {
+    "turma": "23C",
+    "n_matricula": "3",
+    "nome": "Pedro",
+    "disciplina": "Física",
+    "confirmacao_presenca": "Sim ou nao",
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+];
+
+var jsonNotas =
+[
+  {
+    "n_matricula": "1",
+    "nome": "João",
+    "disciplina": "Matemática",
+    "nota": "10",	
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+  {
+    "n_matricula": "2",
+    "nome": "Maria",
+    "disciplina": "Português",
+    "nota": "10",
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+  {
+    "n_matricula": "3",
+    "nome": "Pedro",
+    "disciplina": "Física",
+    "nota": "10",
+    "docente": "Prof. Marcos",
+    "data": "01/01/2019",
+  },
+];
+
+var jsonRelatorios =
+[
+  {
+    "turma": "23A",
+    "n_matricula": "1",
+    "nome": "João",
+    "disciplina": "Matemática",
+    "nota": "10, 5, 3.0",	
+    "atividades": "2.0",
+    "docente": "Prof. Marcos",
+  },
+  {
+    "turma": "23B",
+    "n_matricula": "2",
+    "nome": "Maria",
+    "disciplina": "Português",
+    "nota": "10, 5, 3.0",	
+    "atividades": "2.0",
+    "docente": "Prof. Marcos",
+  },
+  {
+    "turma": "23C",
+    "n_matricula": "3",
+    "nome": "Pedro",
+    "disciplina": "Física",
+    "nota": "10, 5, 3.0",	
+    "atividades": "2.0",
+    "docente": "Prof. Marcos",
+  },
+];
+
+var jsonCtrlAtividades =
+[
+  {
+    "n_matricula": "1",
+    "nome": "João",
+    "atividade_extra": "Algebra 1 40hrs",
+    "docente": "Prof. Marcos",
+    "status": "Aprovado",
+  },
+  {
+    "turma": "23B",
+    "n_matricula": "2",
+    "nome": "Maria",
+    "atividade_extra": "Algebra 1 40hrs",
+    "docente": "Prof. Marcos",
+    "status": "Pendente",
+  },
+  {
+    "turma": "23C",
+    "n_matricula": "3",
+    "nome": "Pedro",
+    "atividade_extra": "Algebra 1 40hrs",
+    "docente": "Prof. Marcos",
+    "status": "Aprovado",
   },
 ];
